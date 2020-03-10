d63ace06095bf819c732a5ed1ee1eb0ab1f7a643
Damien Courtois <damien.courtois@gmail.com>
2020-03-10 11:36:21 +0100

    fixed a bug when generating a project without cu after having already generted once with cu
    basically if the generated compilation units files are in a path that's used with "files", the cu were included as normal files. Now even if the compilation units are not enabled for a given project, any potential compilation unit file is filtered out.

--- compilationunit.lua
+++ compilationunit.lua
@@ -29,8 +29,8 @@ premake.extensions.compilationunit = {
 --
 function premake.extensions.compilationunit.customBakeFiles(base, prj)
 
-	-- if compilation units are disabled for this project, do nothing
-	if prj.compilationunitenabled ~= true or prj.external == true then
+	-- do nothing for external projects
+	if prj.external == true then
 		return base(prj)
 	end
 
@@ -43,9 +43,6 @@ function premake.extensions.compilationunit.customBakeFiles(base, prj)
 	-- run Premake.
 	for cfg in project.eachconfig(prj) do
 
-		-- initialize the compilation unit structure for this config
-		cu.compilationunits[cfg] = {}
-
 		-- remove the previous compilation units
 		for i = #cfg.files, 1, -1 do
 			if cu.isCompilationUnit(cfg, cfg.files[i]) then
@@ -53,37 +50,51 @@ function premake.extensions.compilationunit.customBakeFiles(base, prj)
 			end
 		end
 
-		-- the indices of the files that must be included in the compilation units
-		local sourceindexforcu = {}
+		-- stop there when compilation units are disabled
+		if prj.compilationunitenabled == true then
 
-		-- store the list of files for a later building of the actual compilation unit files
-		for i = #cfg.files, 1, -1 do
-			local filename = cfg.files[i]
-			if cu.isIncludedInCompilationUnit(cfg, filename) == true then
-				table.insert(cu.compilationunits[cfg], filename)
-				table.insert(sourceindexforcu, i)
+			-- initialize the compilation unit structure for this config
+			cu.compilationunits[cfg] = {}
+
+			-- the indices of the files that must be included in the compilation units
+			local sourceindexforcu = {}
+
+			-- store the list of files for a later building of the actual compilation unit files
+			for i = #cfg.files, 1, -1 do
+				local filename = cfg.files[i]
+				if cu.isIncludedInCompilationUnit(cfg, filename) == true then
+					table.insert(cu.compilationunits[cfg], filename)
+					table.insert(sourceindexforcu, i)
+				end
 			end
+
+			-- remove the original source files from the project
+			if cfg.compilationunitsonly then
+				table.foreachi(sourceindexforcu, function(i)
+					table.remove(cfg.files, i)
+				end)
+			end
+
+			-- store the compilation unit folder in the config
+			if cfg._compilationUnitDir == nil then
+				cfg._compilationUnitDir = cu.getCompilationUnitDir(cfg)
+			end
+
+			-- add the compilation units for premake.
+			-- note: avoid generating empty compilation units if we have less files in the project
+			-- than the number of compilation units.
+			local count = math.min(#cu.compilationunits[cfg], cu.numcompilationunits)
+			for i = 1, count do
+				table.insert(cfg.files, path.join(cfg._compilationUnitDir, cu.getCompilationUnitName(cfg, i)))
+			end
+
 		end
 
-		-- remove the original source files from the project
-		if cfg.compilationunitsonly then
-			table.foreachi(sourceindexforcu, function(i)
-				table.remove(cfg.files, i)
-			end)
-		end
+	end
 
-		-- store the compilation unit folder in the config
-		if cfg._compilationUnitDir == nil then
-			cfg._compilationUnitDir = cu.getCompilationUnitDir(cfg)
-		end
-
-		-- add the compilation units for premake.
-		-- note: avoid generating empty compilation units if we have less files in the project
-		-- than the number of compilation units.
-		local count = math.min(#cu.compilationunits[cfg], cu.numcompilationunits)
-		for i = 1, count do
-			table.insert(cfg.files, path.join(cfg._compilationUnitDir, cu.getCompilationUnitName(cfg, i)))
-		end
+	-- we removed any potential previous compilation units, early out if compilation units are not enabled
+	if prj.compilationunitenabled ~= true then
+		return base(prj)
 	end
 
 	-- second step loop through the configs and generate the compilation units
@@ -256,15 +267,15 @@ function premake.extensions.compilationunit.getCompilationUnitName(cfg, index, s
 
 	local language = cfg.language
 	local extension = nil
-	
+
 	if cfg.compilationunitextensions ~= nil then
 		extension = cfg.compilationunitextensions[language]
 	end
-	
+
 	if extension == nil then
 		extension = iif(language == "C", ".c", ".cpp")
 	end
-	
+
 	return premake.extensions.compilationunit.compilationunitname .. index .. extension
 end
 
@@ -315,5 +326,10 @@ if _OPTIONS["compilationunit"] ~= nil then
 	-- setup the overrides
 	premake.override(premake.oven, "bakeFiles", cu.customBakeFiles)
 	premake.override(premake.fileconfig, "addconfig", cu.customAddFileConfig)
+
+else
+
+	-- still need this to avoid including compilation units of a previous build
+	premake.override(premake.oven, "bakeFiles", premake.extensions.compilationunit.customBakeFiles)
 
 end
